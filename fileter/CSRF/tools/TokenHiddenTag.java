5a68e1d733f650609bc0bb07f95d3d6912212ead
r13ljj <r13ljj@gmail.com>
2013-05-03 18:38:38 +0800

    token hidden tag

---
+++ fileter/CSRF/tools/TokenHiddenTag.java
@@ -0,0 +1,39 @@
+package filter.CSRF.tools;
+
+import filter.loophole.config.Configuration;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class TokenHiddenTag extends TagSupport{
+  private String uri; 
+
+  public String getUri(){
+    reutrn this.uri;
+  }
+
+  public void setUri(String uri) {
+    this.uri = uri;
+  }
+  
+  public int doEndTag() throws JspException {
+    HttpServletRequest request = (HttpServletRequest)this.pageContext.getRequest();
+    HttpServletResponse response = (HttpServletResponse)this.pageContext.getResponse();
+    String token = TokenProcessor.getInstance().generateToken(request, Configuration.getInstance().getTokenName(), request.getContextPath() + this.uri);
+try{
+      out.write("<input type=\"hidden\" value=\"");
+      out.write(token);
+      out.write("\" name=\"");
+      out.write(Configuration.getInstance().getTokenName());
+      out.write("\" />");
+      out.flush();
+ }catch(Exception e){
+    e.printStackTrace();
+ }
+ return EVAL_PAGE;
+  }
+}
